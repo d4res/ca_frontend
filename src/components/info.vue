87af35dd924d7e87ba0ae385402b2804aac3dd8c
dares-VMubuntu <dares_._@outlook.com>
2021-11-28 11:11:10 +0800

    info

--- src/components/info.vue
+++ src/components/info.vue
@@ -1,32 +1,73 @@
 <template>
+<div>
+   <el-row>
+      <el-col :span="2" :offset="8" class="left">
+          指定序列号
+      </el-col>
+      <el-col :span="4">
+        <el-input v-model="serial"> </el-input>
+      </el-col>
+      <el-col :span="2" >
+      <el-button @click="submit">查询</el-button>
+      </el-col>
+  </el-row>
     <el-descriptions
     direction="vertical"
     border
     :column="1"
-  >
-    <el-descriptions-item label="证书版本">{{cert.version}}</el-descriptions-item>
-    <el-descriptions-item label="序列号">{{cert.sn}}</el-descriptions-item>
-    <el-descriptions-item label="签名算法" >{{cert.signalg}}</el-descriptions-item>
-    <el-descriptions-item label="签发者"> {{cert.signer}} </el-descriptions-item>
-    <el-descriptions-item label="有效期"> </el-descriptions-item>
-
-    <el-descriptions-item label="公钥算法"></el-descriptions-item>
-
-
+    v-show="show"
+   >
+    <el-descriptions-item label="证书公钥">{{cert.pub_key}}</el-descriptions-item>
+    <el-descriptions-item label="证书序列号">{{cert.serial}}</el-descriptions-item>
+    <el-descriptions-item label="国家" > {{cert.subjectName.C}}</el-descriptions-item>
+    <el-descriptions-item label="省份"> {{cert.subjectName.ST}} </el-descriptions-item>
+    <el-descriptions-item label="城市"> {{cert.subjectName.L}}</el-descriptions-item>
+    <el-descriptions-item label="组织"> {{cert.subjectName.O}}</el-descriptions-item>
   </el-descriptions>
+</div>
 </template>
 
 <script>
+import axios from 'axios'
+import Cookies from 'js-cookie'
+import msg from '../js/msg';
+const url = location.origin + "/ca/info";
 export default {
     name: 'info',
     data() {
         return {
             cert: {
-                version: '',
-                sn: '',
-                signalg: '',
-                signer: ''
-            }
+                pub_key: '',
+                serial: '',
+                subjectName: {
+                    C: '',
+                    CN: '',
+                    L: '',
+                    O:'',
+                    ST: ''
+                }
+            },
+            show: false,
+            serial: ''
+        }
+    },
+    methods: {
+        submit() {
+            axios.post(url, {"serial": this.serial}, {
+                headers: {
+                    "X-CSRF-TOKEN": Cookies.get("X-CSRF-TOKEN"),
+                },
+                withCredentials: true
+            }).then((response) => {
+                if (response.data.error == 1) {
+                    msg.msFail("failed");
+                } else {
+                    this.cert = response.data;
+                    console.log(this.cert);
+                    console.log(response.data);
+                    this.show = true;
+                }
+            })
         }
     }
 }
